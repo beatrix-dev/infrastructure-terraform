9e285212d0f48fac20d8aaa9e939cf6388e8f06f
mosesv23 <56962648+mosesv23@users.noreply.github.com>
2026-04-16 13:36:43 +0200

    fixed vpc config file

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,22 +1,18 @@
 resource "aws_vpc" "vpc" {
 cidr_block = "10.10.0.0/16"
 tags = {
-  name = "${var.client}-vpc"
+  Name = "${var.client}-vpc"
 }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
   tags = {
-    name = "${var.client}-vpc"
+    Name = "${var.client}-igw"
   }
 }
 
-resource "aws_internet_gateway_attachment" "igw-attached" {
-    internet_gateway_id = aws_internet_gateway.igw.id
-    vpc_id = aws_vpc.vpc.id
-  
-}
+
 
 #### Subnets #######
 
@@ -30,17 +26,27 @@ resource "aws_subnet" "public-1b" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = "${var.cidr}.2.0/24"
     availability_zone = "eu-west-1b"
+    tags = {
+      Name = "${var.client}-public1b"
+    }
 }
 
 resource "aws_subnet" "private-1a" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = "${var.cidr}.3.0/24"
     availability_zone = "eu-west-1a"
+    tags = {
+      Name = "${var.client}-private1a"
+    }
 }
+    
 
 resource "aws_subnet" "private-1b" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = "${var.cidr}.4.0/24"
     availability_zone = "eu-west-1b"
+    tags = {
+      Name = "${var.client}-private1b"
+    }
 }
 
