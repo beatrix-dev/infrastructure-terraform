f1f5c2fd6fb1dce09ccb1ea9a3ea370db0ac1046
Romano Chesline Moses <romanoawstraining@gmail.com>
2026-04-16 14:38:22 +0200

    adding remote state config (#2)
    * adding remote state config
    
    * fixed remote state config
    
    * fixed remote state config again
    
    * fixed remote state config again part2
    
    * fixed remote state config again part3
    
    * fixed remote state config again part4
    
    ---------
    
    Co-authored-by: mosesv23 <56962648+mosesv23@users.noreply.github.com>

---
+++ vpc/outputs.tf
@@ -0,0 +1,17 @@
+output "vpc" {
+value = aws_vpc.vpc.id
+}
+
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.public-1a.id,
+    aws_subnet.public-1b.id
+  ]
+}
+
+output "private_subnet_ids" {
+  value = [
+    aws_subnet.private-1a.id,
+    aws_subnet.private-1b.id
+  ]
+}
