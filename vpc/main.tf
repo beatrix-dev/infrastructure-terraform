846f2a1cd9156e80110a6a8b098d2859b2c25d84
mosesv23 <56962648+mosesv23@users.noreply.github.com>
2026-04-16 14:36:29 +0200

    fixed remote state config again part4

--- vpc/main.tf
+++ vpc/main.tf
@@ -56,7 +56,7 @@ resource "aws_subnet" "private-1b" {
 resource "aws_route_table" "public_rt" {
     vpc_id = aws_vpc.vpc.id
 
-    route = {
+    route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.igw.id
     }
