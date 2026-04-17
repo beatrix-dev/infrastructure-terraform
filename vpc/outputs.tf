7caed6a901bee2a403bd71b70fd0d9c3a5c81a8e
mosesv23 <56962648+mosesv23@users.noreply.github.com>
2026-04-17 08:54:51 +0200

    fixed vpc config

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -4,14 +4,14 @@ output "vpc" {
 
 output "public_subnet_ids" {
   value = [
-    aws_subnet.public-1a.id,
-    aws_subnet.public-1b.id
+    aws_subnet.public_1a.id,
+    aws_subnet.public_1b.id
   ]
 }
 
 output "private_subnet_ids" {
   value = [
     aws_subnet.private_1a.id,
-    aws_subnet.private-1b.id
+    aws_subnet.private_1b.id
   ]
 }
