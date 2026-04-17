24222e379be96711905986f1be8301c49f1a5b6c
mosesv23 <56962648+mosesv23@users.noreply.github.com>
2026-04-17 16:39:06 +0200

    fixing vpc outputs

---
+++ ec2/main.tf
@@ -0,0 +1,30 @@
+/* ------ EC2 AMAZON LINUX AMI  */
+
+data "aws_ami" "al2023" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"] # Matches Amazon Linux 2023
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+
+
+
+
+/* ------ EC2 Instance Config */
+
+resource "aws_instance" "amazon_linux_instance" {
+  subnet_id = data.terraform_remote_state.vpc.outputs.public_subnet_1a.id
+  ami = data.aws_ami.al2023.id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "my-instance"
+  }
+
+}
