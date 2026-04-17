096dc3db5f8df6ee2239fc814c7d11a4bb5d5d66
Romano Chesline Moses <romanoawstraining@gmail.com>
2026-04-17 08:07:08 +0200

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -93,7 +93,10 @@ resource "aws_route_table_association" "public_rt_association_1b" {
 
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.vpc.id
-  route {}
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 }
 
 resource "aws_network_acl" "private_nacl" {
