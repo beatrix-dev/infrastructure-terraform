d4ffc889fe9cef05ec4fe7f19dfee8c93a72f118
Romano Chesline Moses <romanoawstraining@gmail.com>
2026-04-17 16:34:09 +0200

    Update workflow (#5)
    * adding ec2 instance
    
    * fixed vpc config
    
    * update workflow file
    
    * update workflow file 2
    
    * update workflow file 3
    
    * fixed the workflow and the outputs in vpc/main.tf
    
    ---------
    
    Co-authored-by: mosesv23 <56962648+mosesv23@users.noreply.github.com>

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -3,15 +3,39 @@ output "vpc" {
 }
 
 output "public_subnet_ids" {
-  value = [
-    aws_subnet.public_1a.id,
-    aws_subnet.public_1b.id
-  ]
+  value = {
+    public_subnet_1a = aws_subnet.public_1a.id,
+    public_subnet_1b = aws_subnet.public_1b.id
+  }
 }
 
 output "private_subnet_ids" {
-  value = [
-    aws_subnet.private_1a.id,
-    aws_subnet.private_1b.id
-  ]
+  value = {
+    private_subnet_1a = aws_subnet.private_1a.id,
+    private_subnet_1b = aws_subnet.private_1b.id
+  }
 }
+
+
+/* 
+
+output "public_subnets" {
+  value = {
+    alb_1a      = aws_subnet.alb_1a.id
+    alb_1b      = aws_subnet.alb_1b.id
+  }
+}
+
+output "private_subnets" {
+  value = {
+    frontend_1a = aws_subnet.frontend_1a.id
+    backend_1a  = aws_subnet.backend_1a.id
+    rds_1a      = aws_subnet.rds_1a.id
+    efs_1a      = aws_subnet.efs_1a.id
+  }
+}
+
+
+
+
+*/
