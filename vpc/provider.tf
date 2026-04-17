2035151f1b701d598ef975d4c7cbf329da4205fb
mosesv23 <56962648+mosesv23@users.noreply.github.com>
2026-04-17 07:40:07 +0200

    adding private route table

--- vpc/provider.tf
+++ vpc/provider.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "6.40.0"
+      source  = "hashicorp/aws"
+      version = "6.40.0"
     }
   }
 }
 
 provider "aws" {
-region = "eu-west-1"
+  region = "eu-west-1"
 }
 
 terraform {
   backend "s3" {
     bucket = "romano-terraform-state-727646468320-eu-west-1-an"
-    key    = "beatrix"
+    key    = "beatrix/"
     region = "eu-west-1"
   }
 }
